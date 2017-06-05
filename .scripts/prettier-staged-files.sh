fe98bbd353deeddcc358013ea088a607191f69b3
Andre Staltz <andre@staltz.com>
2017-06-05 13:16:09 +0300

    chore(META): minor cosmetic improvement to prettier workflow

--- .scripts/prettier-staged-files.sh
+++ .scripts/prettier-staged-files.sh
@@ -6,10 +6,12 @@
 
 tsfiles=$(git diff --cached --name-only --diff-filter=ACM | grep '\.ts\?$' | tr '\n' ' ')
 
+echo "Formatting source code files with prettier..."
 for f in $tsfiles
 do
-  echo "Formatting $f with prettier"
   $(dirname $0)/../node_modules/.bin/prettier --write \
   --single-quote --no-bracket-spacing --trailing-comma=all $f;
   git add $f
 done
+echo "✓ done"
+echo ""
